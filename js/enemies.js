894cbb41eaf4914ae88d125e4ffb2fc3916eb561
Gonzalo Delgado <mail@gonzalodelgado.com>
2020-10-10 08:29:26 -0300

    Update enemy AI with steering vehicle behavior.

--- js/enemies.js
+++ js/enemies.js
@@ -8,7 +8,7 @@ export class Enemy extends Entity {
   constructor(x, y) {
 	super('enemy');
 	this.hp = 1;
-	this.speed = 80;
+	this.speed = 50;
 	this.width = 14;
 	this.height = 26;
 	this.needsCollisionCheck = true;
@@ -23,8 +23,8 @@ export class Enemy extends Entity {
 	this.vel = {x: 0, y: 0};
 	this.currentAnimation = this.animations.walk;
 	this.currentAnimation.play();
-	this.attackDistance = 10;
-	this.disperseTimer = 1;
+	this.attackDistance = 20;
+	this.steerTimer = Math.random()*0.5 + 0.5;
   }
 
   draw() {
@@ -35,17 +35,11 @@ export class Enemy extends Entity {
   }
 
   update(dt) {
-	this.currentAnimation.update(dt);
-	if (this.disperseTimer > 0) {
-	  const disperseVel = {x: Math.random()*this.speed,  y: Math.random()*this.speed};
-	  this.disperseTimer -= dt;
-	  this.pos.x += Math.round(disperseVel.x*dt);
-	  this.pos.y += Math.round(disperseVel.y*dt);
-	  return;
-	}
-	// find closest player
-	let closestPlayer = null;
 	let dist, minDist = Number.MAX_SAFE_INTEGER;
+	this.currentAnimation.update(dt);
+	let closestPlayer = null;
+	const steer = {x: 0, y: 0};
+	// find closest player
 	for (const player of [...entitiesManager.liveEntities].filter(e => e.type == "player")) {
 	  dist = Math.sqrt(Math.pow(player.pos.x - this.pos.x, 2) + Math.pow(player.pos.y - this.pos.y, 2));
 	  if (dist < minDist) {
@@ -53,31 +47,46 @@ export class Enemy extends Entity {
 		closestPlayer = player;
 	  }
 	}
-	if (closestPlayer === null) {
-	  // console.error('Could not find player closest to %s', this);
-	  return;
+	if (this.steerTimer <= 0) {
+	  // Steering vehicle behavior, based on https://natureofcode.com/book/chapter-6-autonomous-agents/
+	  const desiredVel = {x: 0, y: 0};
+	  if (closestPlayer === null) {
+		console.error('Could not find player closest to %s', this);
+	  } else {
+		desiredVel.x = closestPlayer.pos.x - this.pos.x;
+		desiredVel.y = closestPlayer.pos.y - this.pos.y;
+		const mag = Math.sqrt(Math.pow(desiredVel.x, 2) + Math.pow(desiredVel.y, 2));
+		desiredVel.x = this.speed*desiredVel.x/mag;
+		desiredVel.y = this.speed*desiredVel.y/mag;
+	  }
+	  steer.x = desiredVel.x - this.vel.x;
+	  steer.y = desiredVel.y - this.vel.y;
+	  this.steerTimer = Math.random()*0.5 + 1;
+	} else {
+	  this.steerTimer -= dt;
 	}
+
 	if (minDist < this.attackDistance) {
 	  // attack!
 	} else {
-	  // move towards it at some speed
-	  const dir = {
-		x: closestPlayer.pos.x - this.pos.x,
-		y: closestPlayer.pos.y - this.pos.y
-	  };
-	  const mag = Math.sqrt(Math.pow(dir.x, 2) + Math.pow(dir.y, 2));
-	  dir.x = dir.x/mag;
-	  dir.y = dir.y/mag;
-	  const vel = {
-		x: dt*this.speed*dir.x,
-		y: dt*this.speed*dir.y
-	  };
-	  if (Math.abs(vel.x) > 0 && Math.abs(vel.y) > 0) {
-		vel.x *= Math.SQRT1_2;
-		vel.y *= Math.SQRT1_2;
+	  // move towards player
+	  this.vel.x += steer.x;
+	  this.vel.y += steer.y;
+	  this.pos.x += Math.round(this.vel.x*dt);
+	  this.pos.y += Math.round(this.vel.y*dt);
+
+	  if (this.pos.x < 0) {
+		this.pos.x = 0;
 	  }
-	  this.pos.x += Math.round(vel.x*(Math.random() + 1));
-	  this.pos.y += Math.round(vel.y*(Math.random() + 1));
+	  if (this.pos.y < 0) {
+		this.pos.y = 0;
+	  }
+	  if (this.pos.x > canvasData.canvas.width - this.width) {
+		this.pos.x = canvasData.canvas.width - this.width;
+	  }
+	  if (this.pos.y > canvasData.canvas.height - this.height) {
+		this.pos.y = canvasData.canvas.height - this.height;
+	  }
 	}
 	super.update(dt);
   }
